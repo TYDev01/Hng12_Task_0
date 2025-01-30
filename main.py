de872816ad0d489453464209e4347affaa990385
TYDev <anthonychinaemeru84@yahoo.com>
2025-01-30 02:39:42 -0500

    Fix: Port Binding

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import os
+PORT = int(os.environ.get("PORT", 8000))
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=PORT, reload=True)
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from endpoints.endpoint import router as info_router
